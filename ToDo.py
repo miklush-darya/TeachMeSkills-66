f45fa0c72cf87587b19ca4d57925b41fba7a4d5e
miklush-darya <71452291+miklush-darya@users.noreply.github.com>
2022-02-20 23:10:20 +0300

    Update ToDo.py
    task manager with file

--- ToDo.py
+++ ToDo.py
@@ -1,6 +1,26 @@
 
-tasks = {1: 'do homework', 2: 'read book'}
 done_tasks = {}
+
+f = open('Tasks_Manager.txt', 'r')
+q=0
+for i in f:
+    q+=1
+
+f.seek(0)
+def read_str (f):
+    rear_f = f.readline()
+    rear_word = rear_f.split()
+    return rear_word
+
+a=0
+tasks = {}
+for i in range(q):
+    a+=1
+    x= read_str (f)
+    y = "".join(map(str, x))
+    tasks[a] = y
+print(tasks)
+
 
 def repeat_dec(func):
     def vnutr_funk():
@@ -11,25 +31,27 @@ def repeat_dec(func):
                 continue
             else:
                 break
-        # print ('')
     return vnutr_funk
 
 # добавить задачу
 @repeat_dec
 def add_task():
-    # global tasks
+    
     tas_k = input('enter task: ')
-    for i in tasks.keys():
-        p = i
-        # if tas_k in tasks[i]:
-            # print ('задача уже есть')
-    tasks[p+1] = tas_k
-    print (tasks)
+    if tasks == {}:        #для записи в пустой файл
+        tasks[1]= tas_k
+        print (tasks)
+    else:
+        for i in tasks.keys():
+            p = i
+            # if tas_k in tasks[i]:
+                # print ('задача уже есть')
+        tasks[p+1] = tas_k
+        print (tasks)
 
 # удалить задачу
 @repeat_dec
 def del_task():
-    # global tasks
     print(tasks)
     d = int(input('enter number of task to be deleted:'))
     # for i in tasks:
@@ -43,6 +65,7 @@ def del_task():
 # выполненые задачи
 @repeat_dec
 def done_task():
+    print (tasks)
     do = list(map(int, input('enter numbers of tasks that done: ').split()))
 
     for i in do:
@@ -53,12 +76,12 @@ def done_task():
         d_f = f'{done_tasks[i]} - done'
         print(d_f)
 
-    print('tasks -', tasks)
     print('done_tasks -', done_tasks)
 
 # изменить задачу
 @repeat_dec
 def change_task():
+    print (tasks)
     c = int(input('enter number of task you want to change:'))
     h = input('make changes: ')
     tasks[c] = h
@@ -101,6 +124,12 @@ while True:
         change_task()
     elif numb_menu == 4:
         done_task()
+        ToDo_done = open('Tasks_Manager_done.txt', 'a')
+        for k in done_tasks.values():
+            n="".join(map(str, k))
+            record_done = f'{n}\n'
+            ToDo_done.write(record_done)
+        ToDo_done.close()
     elif numb_menu == 5:
         del_task()
     elif numb_menu == 6:
@@ -114,8 +143,13 @@ while True:
         print ('goodbye')
         break
 
+f.close()
 
+ToDo = open('Tasks_Manager.txt', 'w')
 
+for i in tasks.values():
+    m="".join(map(str,i))
+    record = f'{m}\n'
+    ToDo.write(record)
 
-
-
+ToDo.close()
